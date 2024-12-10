7418b28e5df7a0fe781b5bab1acdccebbaa99da3
Fadhil Akbar Djunaedi <fadhilakbar57894@gmail.com>
2024-12-10 16:53:21 +0700

    SAS_backend_fadhil

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import ProductRoute from './router/ProductRouter.js'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import ProductRoute from "./router/ProductRouter.js";
 
 const app = express();
+const uri =
+  "mongodb+srv://Tomsaymon:Fadhilakbar(123456)@sasfadhilakbar.fbw9b.mongodb.net/?retryWrites=true&w=majority&appName=SASfadhilakbar";
 // mongoose.connect("mongodb://localhost:27017/fullstack_db", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-mongoose.connect("mongodb://localhost:27017/restful_db", {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const db=mongoose.connection;
-db.on("error", (error)=>console.log(error))
-db.once("open", ()=> console.log("Success connected..."))
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Success connected..."));
 
-app.use(cors())
-app.use(express.json())
-app.use(ProductRoute)
+app.use(cors());
+app.use(express.json());
+app.use(ProductRoute);
 
-app.listen(6000,()=>console.log("Server running..."))
+app.listen(6000, () => console.log("Server running..."));
